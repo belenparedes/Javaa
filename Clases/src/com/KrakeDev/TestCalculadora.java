b24ca75788cd4078984859f98bca20fb97ae57bc
belenparedes <balupards@gmail.com>
2025-11-26 18:43:47 -0500

    llamarlas funciones al testCalculadora... funcion satisfactoria

--- Clases/src/com/KrakeDev/TestCalculadora.java
+++ Clases/src/com/KrakeDev/TestCalculadora.java
@@ -5,13 +5,25 @@ public class TestCalculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculadora calcul;
+		Calculadora mensaje;
 		int resultadoSuma;
 		double resultadoResta;
+		double resultadoMultiplicacion;
+		double resultadoDividir;
+		double resultadoPromedio;
 		calcul = new Calculadora();
+		mensaje= new Calculadora();
 		resultadoSuma=calcul.sumar(5, 8);
 		resultadoResta=calcul.restar(50, 53);
+		resultadoMultiplicacion=calcul.multiplicar(10, 5);
+		resultadoDividir=calcul.dividir(10, 2);
+		resultadoPromedio=calcul.promediar(10, 8, 9);
 		System.out.println("Resultado Suma: "+resultadoSuma);
 		System.out.println("Resultado Resta: "+resultadoResta);
+		System.out.println("Resultado multiplicacion: "+resultadoMultiplicacion);
+		System.out.println("Resultado Dividir: "+resultadoDividir);
+		System.out.println("Resultado Promedio: "+resultadoPromedio);
+		mensaje.mostrarResultado();
 	}
 
 }
