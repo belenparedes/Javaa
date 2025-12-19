13618fd717cb5a0fa3472c865ed903a6af16f035
belenparedes <balupards@gmail.com>
2025-12-19 17:40:37 -0500

    constructor agregando atributos en clase Producto

--- Clases/src/com/KrakeDev/TestProducto.java
+++ Clases/src/com/KrakeDev/TestProducto.java
@@ -6,11 +6,34 @@ public class TestProducto {
 		// TODO Auto-generated method stub
 		// instanciado y referenciado con las variables productoA, productoB, productoC
 		Producto productoA;
-		productoA = new Producto();
+		productoA = new Producto("Aspirina", "avilia el dolor", 0.87, 12);
 		Producto productoB;
-		productoB = new Producto();
+		productoB = new Producto("Buprex flash", "avilia la fiebre", 2.87, 10);
 		Producto productoC;
-		productoC = new Producto();
+		productoC = new Producto("Sult", "avilia el dolor", 0.85, 8);
+		
+		System.out.println("--------PRODUCTO 1-------");
+		System.out.println("Nombre: " + productoA.getNombre());
+		System.out.println("Descripcion: " + productoA.getDescripcion());
+		System.out.println("Precio:" + productoA.getPrecio());
+		System.out.println("Stock Actual: " + productoA.getStockActual());
+		
+		// MOSTRAR EN PANTALLA LOS PRDUCTOS
+		System.out.println("--------PRODUCTO 2-------");
+		System.out.println("Nombre: " + productoB.getNombre());
+		System.out.println("Descripcion: " + productoB.getDescripcion());
+		System.out.println("Precio:" + productoB.getPrecio());
+		System.out.println("Stock Actual: " + productoB.getStockActual());
+
+		// MOSTRAR EN PANTALLA LOS PRDUCTOS
+		System.out.println("--------PRODUCTO 3-------");
+		System.out.println("Nombre: " + productoC.getNombre());
+		System.out.println("Descripcion: " + productoC.getDescripcion());
+		System.out.println("Precio:" + productoC.getPrecio());
+		System.out.println("Stock Actual: " + productoC.getStockActual());
+
+		
+		
 		// modificar los productos
 		// productoA
 		productoA.setNombre("paracetamol");
