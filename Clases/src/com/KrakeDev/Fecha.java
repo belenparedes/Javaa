0f7f9c6d8adf1f247f292b7a4d27e2ca94d33649
belenparedes <balupards@gmail.com>
2025-12-19 17:32:53 -0500

    constructor agregando atributos en clase Fecha

--- Clases/src/com/KrakeDev/Fecha.java
+++ Clases/src/com/KrakeDev/Fecha.java
@@ -5,6 +5,13 @@ public class Fecha {
 	private int anio;
 	private int mes;
 	private int dia;
+	public Fecha(int anio, int mes, int dia)
+	{
+		this.anio=anio;
+		this.mes=mes;
+		this.dia=dia;
+	}
+	
 	public int getAnio() {
 		return anio;
 	}
